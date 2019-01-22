1bbb3bad071849ed7ba5f1afdaff1deb6c600db0
Tim Crall <timcrall@gmail.com>
2019-01-22 13:55:11 -0800

    Because spelling is important

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 set -x
 cat k8s/deployment.tmpl.yaml | sed "s/{{BUILD_NUMBER}}/34/g" > deployment.yaml
 cat k8s/service.tmpl.yaml | sed "s/{{BUILD_NUMBER}}/34/g" > service.yaml
-kubectl apply -f deployent.yaml
+kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
